607f5cf1c1e9c3f52c08dcb122ab14db9aa9b177
sinmu <3038267725@qq.com>
2024-04-07 19:18:16 +0800

    完善导航栏

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Cool",
+  title: "CoolMe",
   description: "向往自由的追风人",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页', link: '/' },
     ],
-
     sidebar: [
       {
         text: 'Examples',
@@ -22,9 +20,10 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      // { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: {svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Gmail</title><path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>'}, link: 'mailto:3459917374@qq.com' },
     ]
   },
+  head: [['link', { rel: 'icon', type: 'image/x-icon',href: '/coolme.ico' }]],
   cleanUrls: true,
   outDir: '../dist'
 })
